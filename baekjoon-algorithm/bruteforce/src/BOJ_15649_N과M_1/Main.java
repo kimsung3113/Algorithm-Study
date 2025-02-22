484295573ac1e1dd1ebc653a8865f37817bd9e84
kimsung3113 <kimsung1017@naver.com>
2025-02-22 22:11:22 +0900

    boj 15649, 15651 solve

--- baekjoon-algorithm/bruteforce/src/BOJ_15649_N과M_1/Main.java
+++ baekjoon-algorithm/bruteforce/src/BOJ_15649_N과M_1/Main.java
@@ -1,10 +1,11 @@
-package BOJ_15651_N과M_3;
+package BOJ_15649_N과M_1;
 
 import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+
     static StringBuilder sb = new StringBuilder();
 
     static void input() {
@@ -18,22 +19,24 @@ public class Main {
     static int[] selected;
 
     // Recurrence Function (재귀 함수)
-    // 만약 M 개를 전부 고름   => 조건에 맞는 탐색을 한 가지 성공한 것!
-    // 아직 M 개를 고르지 않음 => k 번째부터 M번째 원소를 조건에 맞게 고르는 모든 방법을 시도한다.
     static void rec_func(int k) {
-        if (k == M + 1) { // 다 골랐다!
-            // selected[1...M] 배열이 새롭게 탐색된 결과
+        if (k == M + 1) {
             for (int i = 1; i <= M; i++) sb.append(selected[i]).append(' ');
             sb.append('\n');
         } else {
             for (int cand = 1; cand <= N; cand++) {
-                // k 번째에 cand 가 올 수 있으면
-                selected[k] = cand; // selected[2] -> 2 , selected[2] -> 3, selected[2] -> 4 selected[2] -> 0
-                // selected[1] = 2 , selected[2] = 1, selected[2] = 2, selected[2] = 3, selected[2] = 4 selected[2] -> 0
-                // k+1 번부터 M 번까지 잘 채워주는 함수를 호출해준다.
-                rec_func(k + 1);
-                System.out.println("selected array 표시 : " + Arrays.toString(selected));
-                selected[k] = 0;
+
+                boolean isUsed = false;
+                for (int i = 1; i < k; i++) {
+                    if (cand == selected[i]) {
+                        isUsed = true;
+                    }
+                }
+                if (!isUsed) {
+                    selected[k] = cand;
+                    rec_func(k + 1);
+                    selected[k] = 0;
+                }
             }
         }
     }
@@ -93,4 +96,3 @@ public class Main {
         }
     }
 }
-
